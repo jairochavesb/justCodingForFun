a55dcac365c237c1f02a5bbb84b226b9b3602e2d
Jairo Chaves Brenes <jc@lnx314.jlabs.local>
2025-02-09 16:35:03 -0600

    asdad

--- trash_bin/main.cpp
+++ trash_bin/main.cpp
@@ -1,19 +1,77 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <format>
+#include <filesystem>
+#include <system_error>
+#include <vector>
+#include <unistd.h>
+
+namespace fs = std::filesystem;
+
+using std::cout, std::endl, std::cerr;
+using std::string, std::string_view, fs::path, std::error_code;
+using std::vector;
+
+const string TRASH_FILES {"/.safe_rm/files/"};
+const string TRASH_INFO {"/.safe_rm/info/"};
+
+void list_trash(string_view trash_info) {
+    for (auto f : fs::directory_iterator(trash_info)) {
+        cout << f.path().filename() << endl;
+    }
+
+}
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    char *home {getenv("HOME")};
+    if (!home) {
+        cerr << "can't get user home dir" << endl;
+        return 1;
+    }
 
-    // Set up code that uses the Qt event loop here.
-    // Call a.quit() or a.exit() to quit the application.
-    // A not very useful example would be including
-    // #include <QTimer>
-    // near the top of the file and calling
-    // QTimer::singleShot(5000, &a, &QCoreApplication::quit);
-    // which quits the application after 5 seconds.
+    error_code ec {};
 
-    // If you do not need a running Qt event loop, remove the call
-    // to a.exec() or use the Non-Qt Plain C++ Application template.
+    string trash_path {format("{}{}", home, TRASH_FILES)};
 
-    return a.exec();
+    if (!fs::exists(trash_path)) {
+
+            fs::create_directories(TRASH_FILES, ec);
+            if (ec) {
+                cerr << "can't create trash dir '" << TRASH_FILES << "': " << ec.message() << endl;
+                return 1;
+            }
+
+            fs::create_directories(TRASH_INFO, ec);
+            if (ec) {
+                cerr << "can't create trash dir '" << TRASH_INFO << "': " << ec.message() << endl;
+                return 1;
+            }
+        }
+
+    int opt {0};
+    while((opt = getopt(argc, argv, "elR:")) != -1) {
+        switch (opt) {
+        case 'e':
+
+            cout << "emprtying trash folders..." << endl;
+            list_trash(TRASH_INFO);
+
+            break;
+
+        case 'l':
+            cout << "list trashed files" << endl;
+            break;
+
+        case 'R':
+            for(int i {optind-1}; i < argc; i++) {
+                cout << "recovering file " << argv[i] << endl;
+            }
+
+            break;
+
+        default:
+            break;
+        }
+    }
 }
